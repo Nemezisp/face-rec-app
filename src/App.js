bc821bfbfbce347d1637f4633d46409078ee295f
Nemezisp <p.jarmula5@gmail.com>
2022-04-12 15:30:01 +0200

    added PWA implementation

--- src/App.js
+++ src/App.js
@@ -109,7 +109,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl:this.state.input})
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://face-rec-app-11.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -119,7 +119,7 @@ class App extends Component {
     .then(response => response.json())
     .then((response) => {
       if (response) {
-        fetch('http://localhost:3000/image', {
+        fetch('https://face-rec-app-11.herokuapp.com/image', {
           method: 'put',
           headers: {'Content-Type': 'application/json'},
           body: JSON.stringify({
--- src/index.js
+++ src/index.js
@@ -4,15 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
+import * as serviceWorker from './service-worker'
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorker.register()
