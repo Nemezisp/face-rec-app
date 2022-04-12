d6df1df35ce89dbdcb60dcda55b4db61438a0bee
Nemezisp <p.jarmula5@gmail.com>
2022-04-12 16:34:44 +0200

    another PWA fix try

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -20,6 +20,7 @@ const isLocalhost = Boolean(
 
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    console.log('hohohoho')
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
@@ -30,6 +31,7 @@ export function register(config) {
     }
 
     window.addEventListener('load', () => {
+      console.log('wohoho')
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
